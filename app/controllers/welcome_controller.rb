893b86ada34fda96e1964b8f61689731123f59a7
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2020-12-04 15:08:35 +0000

    Remove email address input from root

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,50 +1,13 @@
 class WelcomeController < ApplicationController
-  skip_before_action :verify_authenticity_token, only: [:show]
-
   def show
     payload = ApplicationKey.validate_jwt!(params[:jwt]) if params[:jwt]
 
     redirect_to after_login_path(payload, current_user) and return if current_user
-
-    @email = params.dig(:user, :email)
-    if @email
-      if @email.blank?
-        @email_error_message = I18n.t("activerecord.errors.models.user.attributes.email.blank")
-        return
-      end
-
-      unless Devise.email_regexp.match? @email
-        @email_error_message = I18n.t("activerecord.errors.models.user.attributes.email.invalid")
-        return
-      end
-
-      if User.exists?(email: @email)
-        login_state = create_login_state(payload, @email)
-        session[:login_state_id] = login_state.id
-        redirect_to user_session_path
-      elsif Rails.configuration.enable_registration
-        if payload || !Rails.configuration.force_jwt_at_registration
-          registration_state = create_registration_state(payload, @email)
-          session[:registration_state_id] = registration_state.id
-          redirect_to new_user_registration_start_path
-        else
-          render "devise/registrations/transition_checker"
-        end
-      else
-        render "devise/registrations/closed"
-      end
-    end
   end
 
 protected
 
-  def create_registration_state(payload, email)
-    RegistrationState.create!(
-      touched_at: Time.zone.now,
-      state: :start,
-      email: email,
-      previous_url: payload&.dig(:post_register_oauth).presence || params[:previous_url],
-      jwt_payload: payload,
-    )
+  def after_login_path(payload, user)
+    payload&.dig(:post_login_oauth).presence || after_sign_in_path_for(user)
   end
 end
--- spec/requests/welcome_spec.rb
+++ spec/requests/welcome_spec.rb
@@ -1,33 +1,9 @@
 RSpec.describe "welcome" do
   describe "GET" do
-    it "renders the email address form" do
+    it "renders the welcome page" do
       get new_user_session_url
 
-      expect(response.body).to have_content(I18n.t("welcome.show.button.label"))
-    end
-
-    context "email address given" do
-      context "the user exists" do
-        let!(:user) { FactoryBot.create(:user) }
-
-        it "redirects to the login form" do
-          get new_user_session_url(user: { email: user.email })
-          follow_redirect!
-
-          expect(response.body).to have_content(I18n.t("devise.sessions.new.heading"))
-        end
-      end
-
-      context "the user doesn't exist" do
-        before { allow(Rails.configuration).to receive(:force_jwt_at_registration).and_return(false) }
-
-        it "redirects to the registration form" do
-          get new_user_session_url(user: { email: "no-such-user@domain.tld" })
-          follow_redirect!
-
-          expect(response.body).to have_content(I18n.t("devise.registrations.start.fields.password.label"))
-        end
-      end
+      expect(response.body).to have_content(I18n.t("welcome.heading"))
     end
   end
 
